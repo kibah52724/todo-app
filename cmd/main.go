c8405bfed56b137c3bcdb27976800ebdc9ed33c3
kibah52724 <kibah52724@ukgent.com>
2022-12-22 16:02:12 +0500

    updated go.mod

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/kibah52724/todo-app"
+)
 
 func main() {
 	srv := new(todo.Server)
 	if err := srv.Run("8000"); err != nil {
-		log.Fatalf()
+		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }
