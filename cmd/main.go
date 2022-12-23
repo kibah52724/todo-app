3e6cb61d5ab3143fbbdbdd9792b8d03b70764986
kibah52724 <kibah52724@ukgent.com>
2022-12-23 16:53:33 +0500

    implementation of handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/kibah52724/todo-app"
+	"github.com/kibah52724/todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }
